3e004429f91043afe64887a7706b1b96c14e4abf
02572513161 <guilherme.fischmann@senado.leg.br>
2024-03-12 22:17:59 -0300

    Create DTO validations and Auditing

--- src/main/java/com/easybytes/accounts/service/impl/AccountsServiceImpl.java
+++ src/main/java/com/easybytes/accounts/service/impl/AccountsServiceImpl.java
@@ -40,8 +40,6 @@ public class AccountsServiceImpl implements IAccountsService {
             throw new CustomerAlreadyExistsException("Customer already registered with given mobileNumber "
                     +customerDto.getMobileNumber());
         }
-        customer.setCreatedAt(LocalDateTime.now());
-        customer.setCreatedBy("Anonymous");
         Customer savedCustomer = customerRepository.save(customer);
         accountsRepository.save(createNewAccount(savedCustomer));
 
